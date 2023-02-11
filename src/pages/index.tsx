602c76396d39c58be76d322ea9b0bb93ffc08a3f
reedislost <reed@lostfr8.com>
2023-02-11 16:00:22 -0700

    upgrade

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import Head from "next/head";
 
 import { Inter } from "@next/font/google";
-import { Box, Center, VStack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  VStack,
+  Text,
+  HStack,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import Image from "next/image";
 import carton from "../../public/carton.svg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const width = useBreakpointValue({ base: 250, md: 350 });
+  const height = useBreakpointValue({ base: 150, md: 600 });
   return (
     <>
       <Head>
@@ -19,9 +28,17 @@ export default function Home() {
       <Center minH={"75vh"} flexDirection="column">
         <VStack>
           <Box>
-            <Text fontSize={"3xl"}>Warehouse Wiki</Text>
+            <Text fontSize={"3xl"}>Warehouse.Wiki</Text>
           </Box>
-          <Image src={carton} alt={"warehouse wiki"} width={150} height={250} />
+          <Box>
+            <Text fontSize={"3xl"}></Text>
+          </Box>
+          <Image
+            src={carton}
+            alt={"warehouse wiki"}
+            width={width}
+            height={height}
+          />
         </VStack>
       </Center>
     </>
